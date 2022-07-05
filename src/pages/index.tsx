613c87d773fc821e319a6aca20114f56519c1bf7
Jordão Beghetto Massariol <jbmassariol@gmail.com>
2022-07-05 12:34:01 -0300

    Finalizando desafio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { GetStaticProps } from 'next';
+import { format } from 'date-fns';
 
+import Link from 'next/link';
+import { useState } from 'react';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +29,122 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  const [data, setData] = useState<PostPagination>({
+    ...postsPagination,
+    results: postsPagination?.results?.map((e: any) => {
+      const title = String(e?.data?.title[0]?.text || e?.data?.title) || '';
+      const subtitle =
+        String(e?.data?.subtitle[0]?.text || e?.data?.subtitle) || '';
+      const author = String(e?.data?.author[0]?.text || e?.data?.author) || '';
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+      return {
+        ...e,
+        data: {
+          title,
+          subtitle,
+          author,
+        },
+        first_publication_date: format(
+          new Date(e?.first_publication_date),
+          'dd MMM yyyy',
+          {
+            locale: ptBR,
+          }
+        ),
+      };
+    }),
+  });
 
-//   // TODO
-// };
+  function handlePaginate() {
+    fetch(data?.next_page).then(res => {
+      res?.json().then(response => {
+        setData(prev => ({
+          next_page: response?.next_page,
+          results: [
+            ...prev?.results,
+            ...response?.results?.map(e => {
+              const title =
+                String(e?.data?.title[0]?.text || e?.data?.title) || '';
+              const subtitle =
+                String(e?.data?.subtitle[0]?.text || e?.data?.subtitle) || '';
+              const author =
+                String(e?.data?.author[0]?.text || e?.data?.author) || '';
+
+              return {
+                ...e,
+                data: {
+                  title,
+                  subtitle,
+                  author,
+                },
+                first_publication_date: format(
+                  new Date(e?.first_publication_date),
+                  'dd MMM yyyy',
+                  {
+                    locale: ptBR,
+                  }
+                ),
+              };
+            }),
+          ],
+        }));
+      });
+    });
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.posts}>
+        {data?.results?.map(post => {
+          return (
+            <Link key={post?.uid} href={`post/${post?.uid}`}>
+              <div className={styles.post}>
+                <h1>{post?.data?.title}</h1>
+                <p>{post?.data?.subtitle}</p>
+
+                <footer>
+                  <div>
+                    <img src="./calendar.svg" alt="Data" />
+                    <span> {post?.first_publication_date}</span>
+                  </div>
+
+                  <div>
+                    <img src="./user.svg" alt="Autor" />
+                    <span> {post?.data?.author}</span>
+                  </div>
+                </footer>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+
+      {data?.next_page && (
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handlePaginate}
+        >
+          Carregar mais posts
+        </button>
+      )}
+    </div>
+  );
+}
+
+export const getStaticProps = async ctx => {
+  const prismic = getPrismicClient({});
+  const page = await prismic.getByType('desafio', {
+    pageSize: 1,
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: page?.next_page,
+        results: page?.results,
+      },
+    },
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,9 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
-
-import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
@@ -26,20 +27,145 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  const router = useRouter();
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
 
-//   // TODO
-// };
+  const postData: any = post;
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  const formattedPost = {
+    data: {
+      author: postData?.data?.author[0]?.text || postData?.data?.author,
+      banner: { url: postData?.data?.banner?.url || '' },
+      title: postData?.data?.title[0]?.text || postData?.data?.title,
+      content: postData?.data?.content,
+    },
+    first_publication_date: format(
+      new Date(postData?.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+  };
 
-//   // TODO
-// };
+  const wordsReadPerMinute = 200;
+
+  const minutesReduce = formattedPost?.data?.content?.reduce((acc, content) => {
+    let contentWords = 0;
+
+    if (content?.heading?.length === 1) {
+      content?.heading?.forEach(heading => {
+        contentWords += heading?.text?.split(' ')?.length;
+      });
+    } else {
+      contentWords += content?.heading?.split(' ')?.length;
+    }
+
+    content?.body?.forEach(body => {
+      contentWords += body?.text?.split(' ')?.length;
+    });
+
+    return acc + contentWords;
+  }, 0);
+
+  const minutes = Math.ceil(minutesReduce / wordsReadPerMinute);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerImage}>
+        <img src={formattedPost?.data?.banner?.url || '/banner'} alt="Banner" />
+      </div>
+
+      <div className={styles.content}>
+        <header>
+          <h1>{formattedPost?.data?.title}</h1>
+
+          <div className={styles.authorInfo}>
+            <div>
+              <img src="/calendar.svg" alt="Data" />
+              <span> {formattedPost?.first_publication_date}</span>
+            </div>
+
+            <div>
+              <img src="/user.svg" alt="Autor" />
+              <span> {formattedPost?.data?.author}</span>
+            </div>
+
+            <div>
+              <img src="/clock.svg" alt="Autor" />
+              <span> {minutes} min</span>
+            </div>
+          </div>
+        </header>
+
+        {!formattedPost?.data?.content
+          ? 'Carregando...'
+          : formattedPost?.data?.content?.map(content => {
+              return (
+                <section
+                  key={
+                    content?.heading?.length === 1
+                      ? RichText?.asText(content?.heading)
+                      : content?.heading
+                  }
+                >
+                  <div
+                    className={styles.title}
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        content?.heading?.length === 1
+                          ? RichText?.asText(content?.heading)
+                          : content?.heading,
+                    }}
+                  />
+
+                  <p
+                    className={styles.body}
+                    dangerouslySetInnerHTML={{
+                      __html: RichText.asHtml(content?.body),
+                    }}
+                  />
+                </section>
+              );
+            })}
+      </div>
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const page = await prismic.getByType('desafio', {
+    pageSize: 1,
+  });
+
+  const paths = [];
+
+  page?.results?.forEach(post => {
+    paths?.push({
+      params: {
+        slug: post?.uid,
+      },
+    });
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByUID('desafio', String(params?.slug), {});
+
+  return {
+    props: {
+      post: response,
+    },
+  };
+};
